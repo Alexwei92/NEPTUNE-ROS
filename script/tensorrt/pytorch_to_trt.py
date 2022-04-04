903413e533c9791f77146df3c83f0404a8914ad5
Alexwei92 <penwei@ucdavis.edu>
2022-04-04 08:03:57 -0700

    update tensorrt

---
+++ script/tensorrt/pytorch_to_trt.py
@@ -0,0 +1,57 @@
+import torch
+from affordance_net import AffordanceNet
+import tensorrt as trt
+
+from log_utils import timer, logger
+
+trt_logger = trt.Logger(trt.Logger.WARNING)
+trt_runtime = trt.Runtime(trt_logger)
+
+ONNX_FILE_PATH = 'affordance_net.onnx'
+TRT_PATH = 'affordance_net.trt'
+
+def build_engine(onnx_path):
+    with trt.Builder(trt_logger) as builder, builder.create_network(1) as network, trt.OnnxParser(network, trt_logger) as parser:
+        config = builder.create_builder_config()
+        config.max_workspace_size = (1 << 30)
+        # config.set_flag(trt.BuilderFlag.FP16)
+        parser.parse_from_file(onnx_path)        
+        serialized_engine = builder.build_serialized_network(network, config)
+        return serialized_engine
+
+def save_engine(serialized_engine, engine_path):
+    with open(engine_path, 'wb') as f:
+        f.write(serialized_engine)
+
+
+if __name__ == '__main__':
+
+    load_pytorch = timer("Loading Pytorch model")
+    model = AffordanceNet().eval().cuda()
+    # TODO: load trained model parameters
+    # write code here
+    load_pytorch.end()
+
+
+    # Pytorch to onnx
+    pytorch_to_onnx = timer("Convert Pytorch to ONNX file")
+    sample_input = torch.ones((1, 3, 256, 256)).cuda()
+    torch.onnx.export(model, sample_input, ONNX_FILE_PATH, input_names=['input'],
+                        output_names=['output'], export_params=True)
+    pytorch_to_onnx.end()
+
+    # check the onnx model
+    import onnx
+    onnx_model = onnx.load(ONNX_FILE_PATH)
+    onnx.checker.check_model(onnx_model)
+    print(onnx.helper.printable_graph(onnx_model.graph))
+
+    build_trt = timer('Parser ONNX & Build TensorRT Engine')
+    engine = build_engine(ONNX_FILE_PATH)
+    build_trt.end()
+
+    save_trt = timer('Save TensorRT Engine')
+    save_engine(engine, TRT_PATH)
+    save_trt.end()
+
+    print("Successfully generated the .trt file!")
---
+++ script/tensorrt/runtime_compare.py
@@ -0,0 +1,81 @@
+import cv2
+import torch
+import numpy as np
+from torchvision import transforms
+import onnxruntime as ort
+import tensorrt as trt
+import time
+
+from affordance_net import AffordanceNet
+from log_utils import timer, logger
+import common
+
+trt_logger = trt.Logger(trt.Logger.WARNING)
+trt_runtime = trt.Runtime(trt_logger)
+
+ONNX_FILE_PATH = 'affordance_net.onnx'
+TRT_PATH = 'affordance_net.trt'
+
+def load_engine(trt_runtime, engine_path):
+    with open(engine_path, 'rb') as f:
+        serialized_engine  = f.read()
+    engine = trt_runtime.deserialize_cuda_engine(serialized_engine)
+    return engine
+
+if __name__ == '__main__':
+    my_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5), (0.5)),
+    ])
+    
+    img = cv2.imread('sample_image.jpg')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (256, 256))
+    img_tensor = my_transform(img).unsqueeze(0)
+    img_np = np.array(img_tensor)
+
+    # # Pytorch load time
+    # load_pytorch = timer('Load Pytorch Model')
+    # pytorch_model = AffordanceNet().eval().cuda()
+    # load_pytorch.end()
+    
+    # # ONNX load time
+    # load_onnx = timer('Load ONNX Model')
+    # ort_session = ort.InferenceSession(ONNX_FILE_PATH, providers=['CUDAExecutionProvider'])
+    # load_onnx.end()
+
+    # TensorRT load time
+    load_trt = timer('Load TRT Model')
+    engine = load_engine(trt_runtime, TRT_PATH)
+    load_trt.end()
+    inputs, outputs, bindings, stream = common.allocate_buffers(engine)
+    inputs[0].host = img_np
+
+    # # Pytorch infer time
+    # infer_pytorch = timer('Run Pytorch Infer')
+    # with torch.no_grad():
+    #     for i in range(10):
+    #         tic = time.perf_counter()
+    #         pytorch_outputs = pytorch_model(img_tensor.to(torch.device("cuda:0")))
+    #         print(1/(time.perf_counter() - tic))
+    # print("Result: ", pytorch_outputs.cpu().squeeze(0).numpy())
+    # infer_pytorch.end()
+    
+    # # ONNX infer time
+    # infer_onnx = timer('Run ONNX Infer')
+    # for i in range(10):
+    #     tic = time.perf_counter()
+    #     onnx_outputs = ort_session.run(None, {'input': img_np})[0]
+    #     print(1/(time.perf_counter() - tic))
+    # print("Result: ", onnx_outputs[0])
+    # infer_onnx.end()
+
+    # TRT infer time
+    infer_trt = timer('Run TRT infer')
+    with engine.create_execution_context() as context:
+        for i in range(100):
+            tic = time.perf_counter()
+            trt_outputs = common.do_inference(context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)
+            print(1/(time.perf_counter() - tic))
+    print("Result: ", trt_outputs[0])
+    infer_trt.end()
