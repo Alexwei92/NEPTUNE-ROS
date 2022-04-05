bcaf30b98e62a60f21cd2f7d838e9732005bf51d
Peng <penwei@ucdavis.edu>
2022-04-05 15:57:25 -0700

    add fade in to forward mode

--- src/px4_offboard/src/forward_flight.cpp
+++ src/px4_offboard/src/forward_flight.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <math.h>
 #include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Point.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/Header.h>
@@ -9,6 +10,7 @@
 #include <mavros_msgs/State.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <boost/bind.hpp>
 
 #include "common.h"
 #include "math_utils.h"
@@ -20,39 +22,47 @@ class ForwardCtrl
 public:
     ForwardCtrl()
     {
-        target.header = std_msgs::Header();
-        target.header.frame_id = "base_drone";
+        InitializeTarget();
         
         // Publisher
         target_setpoint_pub = nh.advertise<mavros_msgs::PositionTarget>("/mavros/setpoint_raw/local", 1);  
 
         // Subscriber
-        rcin_sub = nh.subscribe<mavros_msgs::RCIn>("/mavros/rc/in", 5, &ForwardCtrl::RCInCallback, this);
-        local_pose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 
-                    5, &ForwardCtrl::LocalPoseCallback, this);
+        state_sub = nh.subscribe<mavros_msgs::State>("/mavros/state", 5, 
+                &ForwardCtrl::StateCallback, this);
+        rcin_sub = nh.subscribe<mavros_msgs::RCIn>("/mavros/rc/in", 5, 
+                boost::bind(&ForwardCtrl::RCInCallback, this, _1, YAW_CHANNEL));
+        local_pose_sub = nh.subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 5,
+                &ForwardCtrl::LocalPoseCallback, this);
     }
 
     void run()
     {
         ros::Rate loop_rate(LOOP_RATE_DEFAULT);
-        ROS_INFO("Start Offboard Mode!");
+        ROS_INFO("Node Started!");
         while (ros::ok()) {
             target.header.stamp = ros::Time::now();
             target.header.seq++;
-            // bitmask
-            target.coordinate_frame = 8; // {MAV_FRAME_BODY_NED:8, MAV_FRAME_LOCAL_NED:1}
-            target.type_mask = 1479; // velocity + yawrate
-            // velocity
-            geometry_msgs::Vector3 velocity_local;
-            velocity_local.x = FORWARD_SPEED;
-            velocity_local.y = 0; 
-            velocity_local.z = 0;
-            if (target.coordinate_frame == 1) {
-                rotate_body_frame_to_NE(velocity_local.x, velocity_local.y, yaw_rad);
+
+            if (current_state.mode == "OFFBOARD") { 
+                // 2 second fade in
+                double ratio = 1.0;
+                double dt = (ros::Time::now() - offboard_start_time).toSec();
+                if (dt < 2.0) { 
+                    ratio = dt / 2.0;
+                }                
+                // velocity
+                geometry_msgs::Vector3 velocity_local;
+                velocity_local.x = FORWARD_SPEED * ratio;
+                velocity_local.y = 0; 
+                velocity_local.z = 0;
+                if (target.coordinate_frame == 1) {
+                    rotate_body_frame_to_NE(velocity_local.x, velocity_local.y, yaw_rad);
+                }
+                target.velocity = velocity_local;
+                // yaw rate
+                target.yaw_rate = -yaw_cmd * MAX_YAWRATE * DEG2RAD;
             }
-            target.velocity = velocity_local;
-            // yaw rate
-            target.yaw_rate = -yaw_cmd * MAX_YAWRATE * DEG2RAD;
             
             target_setpoint_pub.publish(target);
             ros::spinOnce();
@@ -62,8 +72,34 @@ public:
     }
 
 private:
-    void RCInCallback(const mavros_msgs::RCIn::ConstPtr& msg) {
-        float cmd = rc_mapping(msg->channels[YAW_CHANNEL]);
+    void InitializeTarget() {
+        // bitmask
+        target.header.frame_id = "base_drone";
+        target.coordinate_frame = 8; // {MAV_FRAME_BODY_NED:8, MAV_FRAME_LOCAL_NED:1}
+        target.type_mask = 1479; // velocity + yawrate
+        target.position = geometry_msgs::Point();
+        target.velocity = geometry_msgs::Vector3();
+        target.acceleration_or_force = geometry_msgs::Vector3();
+        target.yaw = 0.0;
+        target.yaw_rate = 0.0;
+    }
+
+    void StateCallback(const mavros_msgs::State::ConstPtr& msg) {
+        if (msg->mode == "OFFBOARD" && current_state.mode != "OFFBOARD") {
+            ROS_INFO("Switched to OFFBOARD Mode!");
+            offboard_start_time = ros::Time::now();
+        }
+
+        if (msg->mode != "OFFBOARD" && current_state.mode == "OFFBOARD") {
+            ROS_INFO("Switched to %s Mode!", (msg->mode).c_str());
+            InitializeTarget();
+        }
+        
+        current_state = *msg;
+    }
+    
+    void RCInCallback(const mavros_msgs::RCIn::ConstPtr& msg, int channel_index) {
+        float cmd = rc_mapping(msg->channels[channel_index]);
         yaw_cmd = constrain_float(cmd, -1.0, 1.0);
     }
 
@@ -79,13 +115,17 @@ private:
 private:
     ros::NodeHandle nh;
     ros::Publisher target_setpoint_pub;
+    ros::Subscriber state_sub;
     ros::Subscriber rcin_sub;
     ros::Subscriber local_pose_sub;
 
+    mavros_msgs::State current_state;
     mavros_msgs::PositionTarget target;
     
     float yaw_cmd; // in [-1.0, 1.0]
     float yaw_rad; // Down
+
+    ros::Time offboard_start_time;
 };
 
 int main(int argc, char **argv)
